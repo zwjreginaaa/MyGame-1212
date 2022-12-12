cc3d2711d5a8b1fc73a8ebf68ffdc7cc05f862d6
zwj <2135821058@qq.com>
2022-12-12 22:46:54 +0800

    第一次提交1212

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    
+    private float dirX=0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    private enum MovementState { idle,running,jumping,falling }
+    //private MovementState state = MovementState.idle;
+    /*int wholeNumber = 16;
+    float decimalNumber = 4.54f;
+    string text = "blabla";
+    bool boolean = false;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimation();
+    } 
+
+    private void UpdateAnimation()
+    {
+        MovementState state;
+
+       if(dirX>0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+       else if(dirX<0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+       if(rb.velocity.y>.1f)
+        {
+            state = MovementState.jumping;
+        }
+       else if(rb.velocity.y<-.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+}
